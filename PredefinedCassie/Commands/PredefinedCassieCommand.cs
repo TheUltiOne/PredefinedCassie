8184fb403be7a02dcf3af020d427859a6a1f8843
Raul125 <raul125esp@gmail.com>
2021-09-17 20:53:29 +0200

    No more heart attacks

---
+++ PredefinedCassie/Commands/PredefinedCassieCommand.cs
@@ -0,0 +1,48 @@
+﻿namespace PredefinedCassie.Command
+{
+    using CommandSystem;
+    using Exiled.API.Features;
+    using RemoteAdmin;
+    using System;
+
+    [CommandHandler(typeof(RemoteAdminCommandHandler))]
+    public class PredefinedCassieCommand : ICommand
+    {
+        public string Command { get; } = "predefinedcassie";
+
+        public string[] Aliases { get; } = { "pc" };
+
+        public string Description { get; } = "Executes an Predefined Cassie Command";
+
+        public bool Execute(ArraySegment<string> arguments, ICommandSender sender, out string response)
+        {
+            Player player = Player.Get((sender as PlayerCommandSender).ReferenceHub);
+
+            if (arguments.Count == 0)
+            {
+                string cassies = "";
+                foreach (var cas in Plugin.Singleton.Config.PredefiniedCassies.Keys)
+                {
+                    cassies += $"{cas}\n";
+                }
+
+                response = $"Available cassies: \n{cassies}";
+                return false;
+            }
+
+            string cassie = arguments.At(0);
+
+            if (!Plugin.Singleton.Config.PredefiniedCassies.ContainsKey(cassie))
+            {
+                response = $"The predefined cassie <b>{cassie}</b> isn't valid";
+
+                return false;
+            }
+
+            Cassie.Message(Plugin.Singleton.Config.PredefiniedCassies[cassie], false, Plugin.Singleton.Config.AreCassiesNoisy);
+
+            response = "Cassie sent";
+            return true;
+        }
+    }
+}
