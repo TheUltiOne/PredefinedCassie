4a744f480818aa3542b745d74c37586d061b77fb
Raul125 <71040879+Raul125@users.noreply.github.com>
2021-09-17 21:08:04 +0200

    Update Config.cs

--- PredefinedCassie/Config.cs
+++ PredefinedCassie/Config.cs
@@ -1,5 +1,4 @@
-<<<<<<< master
-﻿namespace PredefinedCassie
+namespace PredefinedCassie
 {
     using Exiled.API.Interfaces;
     using System.Collections.Generic;
@@ -18,28 +17,4 @@
             { "chaos", "no" }
         };
     }
-=======
-﻿namespace PredefinedCassie
-{
-    using Exiled.API.Interfaces;
-    using System.ComponentModel;
-
-    public sealed class Config : IConfig
-    {
-        public bool IsEnabled { get; set; } = true;
-        [Description("If you set it to false, all predefined cassies will not have the starting and ending sound and noise between them")]
-        public bool AreCassiesNoisy { get; private set; } = true;
-        [Description("Example")]
-        public string Cassie1 { get; private set; } = "Containment Breach Detected";
-        public string Cassie2 { get; private set; }
-        public string Cassie3 { get; private set; }
-        public string Cassie4 { get; private set; }
-        public string Cassie5 { get; private set; }
-        public string Cassie6 { get; private set; }
-        public string Cassie7 { get; private set; }
-        public string Cassie8 { get; private set; }
-        public string Cassie9 { get; private set; }
-        public string Cassie10 { get; private set; }
-    }
->>>>>>> master
 }
